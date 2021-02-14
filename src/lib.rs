b320cec1b08ca0b4d1e3c0e15be0c6c9d7efe6c2
Lucas Marques <lucas.rosa@meri.co>
2021-02-14 17:24:34 -0300

    feat: base for prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
+#![recursion_limit="256"]
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
-use chrono::prelude::*;
+// use chrono::prelude::*;
 
 struct Counter {
     link: ComponentLink<Self>,
     value: i64,
-    base_time: DateTime<Local>
+    local_timezone: i32,
+    desired_overlap: u32,
+    overlap: u32,
+    target_timezone: i32
 }
 
 enum Msg {
     AddOne,
-    UpdateBaseTime
+    UpdateLocalTime(i32),
+    UpdateTargetTimezone(i32),
+    UpdateDesiredOverlap(u32)
 }
 
 impl Component for Counter {
@@ -20,19 +26,32 @@ impl Component for Counter {
         Self {
             link,
             value: 0,
-            base_time: Local::now()
+            desired_overlap: 8,
+            overlap: 0,
+            local_timezone: -3,
+            target_timezone: -5
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::AddOne => self.value += 1,
-            Msg::UpdateBaseTime => {
-                ()
+            Msg::UpdateLocalTime(time) => {
+                self.local_timezone = time
+            },
+            Msg::UpdateTargetTimezone(time) => {
+                self.target_timezone = time
+            },
+            Msg::UpdateDesiredOverlap(hours) => {
+                if hours < 0 {
+                    self.desired_overlap = 0
+                } else {
+                    self.desired_overlap = hours
+                }
             }
         }
         true
-    }
+    } 
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
         // Should only return "true" if new properties are different to
@@ -43,17 +62,21 @@ impl Component for Counter {
 
     fn view(&self) -> Html {
         let add_value = self.link.callback(|_| Msg::AddOne);
-        let update_base_time = self.link.callback(|_| Msg::UpdateBaseTime);
+        let update_local_timezone = self.link.callback(|e: InputData| Msg::UpdateLocalTime(e.value.parse().unwrap()));
+        let update_target_timezone = self.link.callback(|e: InputData| Msg::UpdateTargetTimezone(e.value.parse().unwrap()));
+        let update_desired_overlap = self.link.callback(|e: InputData| Msg::UpdateDesiredOverlap(e.value.parse().unwrap()));
         html! {
             <div>
                 <button onclick=add_value>{ "+1" }</button>
                 <p>{ self.value }</p>
-                <p>{"Base time"}</p>
-                <input type="text" placeholder="00:00" onchange=update_base_time value={self.base_time.to_string()}/>
-                <p>{"Overlap"}</p>
-                <input type="number" placeholder="4" />
-                <p>{"SF time"}</p>
-                <input type="text" placeholder="00:00" />
+                <p>{"Base timezone"}</p>
+                <input type="text" placeholder="0" oninput=update_local_timezone value={self.local_timezone}/>
+                <p>{"Target timezone"}</p>
+                <input type="text" placeholder="0" oninput=update_target_timezone value={self.target_timezone}/>
+                <p>{"Desired Overlap (hours)"}</p>
+                <input type="number" placeholder="4" oninput=update_desired_overlap value={self.desired_overlap} />
+                <p>{"Working works overlap (hours)"}</p>
+                <input type="text" placeholder="00:00" readonly=true value={self.overlap}/>
             </div>
         }
     }
