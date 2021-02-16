0eab96b7cdf7ae40e2ea0b879835ee9f7e8b1272
Lucas Marques <lucaskauz@gmail.com>
2021-02-16 08:44:45 -0300

    feat: base fields and calc

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,17 @@ use yew::prelude::*;
 
 struct Counter {
     link: ComponentLink<Self>,
-    value: i64,
     local_timezone: i32,
     desired_overlap: u32,
-    overlap: u32,
+    overlap: i32,
     target_timezone: i32
 }
 
 enum Msg {
-    AddOne,
     UpdateLocalTime(i32),
     UpdateTargetTimezone(i32),
-    UpdateDesiredOverlap(u32)
+    UpdateDesiredOverlap(u32),
+    UpdateOverlap()
 }
 
 impl Component for Counter {
@@ -25,29 +24,35 @@ impl Component for Counter {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            value: 0,
             desired_overlap: 8,
             overlap: 0,
             local_timezone: -3,
-            target_timezone: -5
+            target_timezone: -4
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1,
             Msg::UpdateLocalTime(time) => {
-                self.local_timezone = time
+                self.local_timezone = time;
+                // Msg::UpdateOverlap();
+                self.overlap = self.target_timezone - self.local_timezone;
+            },
+            Msg::UpdateOverlap() => {
+                self.overlap = self.target_timezone - self.local_timezone;
             },
             Msg::UpdateTargetTimezone(time) => {
-                self.target_timezone = time
+                self.target_timezone = time;
+                // Msg::UpdateOverlap();
+                let overlap = self.target_timezone - self.local_timezone;
+                if overlap.is_negative() {
+                    self.overlap = overlap.wrapping_abs();
+                } else {
+                    self.overlap = overlap;
+                }
             },
             Msg::UpdateDesiredOverlap(hours) => {
-                if hours < 0 {
-                    self.desired_overlap = 0
-                } else {
-                    self.desired_overlap = hours
-                }
+                self.desired_overlap = hours
             }
         }
         true
@@ -57,18 +62,17 @@ impl Component for Counter {
         // Should only return "true" if new properties are different to
         // previously received properties.
         // This component has no properties so we will always return "false".
-        false
+        self.overlap = self.target_timezone - self.local_timezone;
+        true
     }
 
     fn view(&self) -> Html {
-        let add_value = self.link.callback(|_| Msg::AddOne);
         let update_local_timezone = self.link.callback(|e: InputData| Msg::UpdateLocalTime(e.value.parse().unwrap()));
         let update_target_timezone = self.link.callback(|e: InputData| Msg::UpdateTargetTimezone(e.value.parse().unwrap()));
         let update_desired_overlap = self.link.callback(|e: InputData| Msg::UpdateDesiredOverlap(e.value.parse().unwrap()));
+        Msg::UpdateOverlap();
         html! {
             <div>
-                <button onclick=add_value>{ "+1" }</button>
-                <p>{ self.value }</p>
                 <p>{"Base timezone"}</p>
                 <input type="text" placeholder="0" oninput=update_local_timezone value={self.local_timezone}/>
                 <p>{"Target timezone"}</p>
@@ -76,7 +80,7 @@ impl Component for Counter {
                 <p>{"Desired Overlap (hours)"}</p>
                 <input type="number" placeholder="4" oninput=update_desired_overlap value={self.desired_overlap} />
                 <p>{"Working works overlap (hours)"}</p>
-                <input type="text" placeholder="00:00" readonly=true value={self.overlap}/>
+                <input type="text" readonly=true value={self.overlap}/>
             </div>
         }
     }
